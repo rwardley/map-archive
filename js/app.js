91e0e5687de6a62ffa28280f45ac5115855f5f9b
Rosemary Wardley <rosemary.wardley@gmail.com>
2018-08-27 16:46:00 -0400

    pushing online

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ var pg = require('pg')
 var format = require('pg-format')
 var PGUSER = 'rosemarywardley'
 var PGDATABASE = 'map_archive'
-//var map_name = 'Belize'
+var router = express.Router()
+//var map_name = 'South Island, New Zealand'
 var year = 1972
 
 var config = {
@@ -17,17 +18,47 @@ var config = {
 var pool = new pg.Pool(config)
 var myClient
 
+app.all('*',function(req,res,next)
+{
+    if (!req.get('Origin')) return next();
+
+    res.set('Access-Control-Allow-Origin','http://localhost:8888');
+    res.set('Access-Control-Allow-Methods','GET,POST');
+    res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
+
+    if ('OPTIONS' == req.method) return res.send(200);
+
+    next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    console.log(req.query.decade)
+      var mapQuery = format('SELECT * from map_table WHERE issue_year = %L', req.query.decade[0])
+  myClient.query(mapQuery, function (err, result) {
+    if (err) {
+      console.log(err)
+    }
+    res.json(result);
+  })
+       
+});
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
 pool.connect(function (err, client, done) {
   if (err) console.log(err)
   app.listen(3000, function () {
     console.log('listening on 3000')
   })
   myClient = client
-  var mapQuery = format('SELECT * from map_table WHERE issue_year = %L', year)
-  myClient.query(mapQuery, function (err, result) {
-    if (err) {
-      console.log(err)
-    }
-    console.log(result)
-  })
+//  var mapQuery = format('SELECT * from map_table WHERE issue_year = %L', year)
+//  myClient.query(mapQuery, function (err, result) {
+//    if (err) {
+//      console.log(err)
+//    }
+//    console.log(result)
+//  })
+    
 })
--- js/main.js
+++ js/main.js
@@ -6,12 +6,22 @@ document.getElementById("submitBtn").addEventListener("click", function(){
        var yearCheckbox = document.querySelector('input[value="' + item + '"]');
        
        if(yearCheckbox.checked) {
-           queryArray.push(item);
+           queryArray.push("decade="+item);
        }
-    
 });
     
-console.log(queryArray)   
+console.log(queryArray)  
+
+var queryDecadeParam = queryArray.join("&")
+console.log(queryDecadeParam)
+
+ $.ajax("http://localhost:3000/api?"+queryDecadeParam,{
+        dataType: "json",
+        crossOrigin: true,
+        success: function(response){console.log(response)
+        
+        }
+    });
 
 //make the ajax query call for everything that ends up in the queryArray +9
 //how do i pass the years that I want into the app.js dynamically
@@ -20,4 +30,4 @@ console.log(queryArray)
 });
 var queryArray = [];
 //make integers
-var decades = [1880, '1890', '1900', '1910', '1920', '1930', '1940', '1950', '1960', '1970', '1980', '1990', '2000', '2010'];
+var decades = [1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010];
